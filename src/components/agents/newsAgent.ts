d25f05d4438af0e3f9b52ab67009854a7ac8c0ab
Vladyslav Kokitko <koks.vladislav277@gmail.com>
2025-12-01 20:02:51 +0100

    news fetching agent

---
+++ src/components/agents/newsAgent.ts
@@ -0,0 +1,33 @@
+const newsApi: string = "https://newsapi.org/v2/everything"
+
+export interface Article {
+    source: {
+        id: string;
+        name: string;
+    }
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+}
+
+export interface NewsData {
+    status: string;
+    totalResults: number;
+    articles: Array<Article>;
+}
+
+const fetchNews = async (keywords: string, apiKey: string) => {
+    try {
+        const response = await fetch(`${newsApi}?q=${keywords}&apiKey=${apiKey}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error. status ${response.status}`);
+        }
+        const data: NewsData = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error while fetching news:', error);
+        throw error;
+    }
+
+}
